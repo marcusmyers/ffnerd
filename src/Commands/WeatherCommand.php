63956c15b199b8321ab23e5e25cd80a1be7222fb
Mark Myers <mmyers@squaremouth.com>
2016-08-24 06:50:28 -0400

    Add nfl teams command
    In order to list out the nfl teams or just one team this commit adds the
    `nflteam` command.

--- src/Commands/WeatherCommand.php
+++ src/Commands/WeatherCommand.php
@@ -4,7 +4,6 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use GuzzleHttp\Client;
 
 class WeatherCommand extends Command {
   public function __construct()
@@ -22,8 +21,8 @@ class WeatherCommand extends Command {
   public function execute(InputInterface $input, OutputInterface $output)
   {
     $api_key = getenv('FFN_API_KEY');
-    $client = new Client();
-    $res = $client->request('GET', "http://www.fantasyfootballnerd.com/service/weather/json/$api_key");
+    $url = self::getFFNServiceUrl() . "/weather/json/$api_key";
+    $res = $this->client->request('GET', $url);
 
     echo $res->getBody();
   }
